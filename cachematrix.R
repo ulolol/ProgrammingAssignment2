7bb08986828f948e3d044ea5959f9189e943da5a
ulolol <57653368+ulolol@users.noreply.github.com>
2020-06-27 16:43:20 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## functions for caching the inverse of a matrix
 ## Write a short comment describing this function
+## Creates a special matrix object with the ability to cache its own inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( matx = matrix() ) {
+        
+        ## Initialize inverse
+        inv <- NULL
+        
+        ## create a method for setting the matrix 
+        set <- function( matrix ) {
+                matx <<- matrix
+                inv <<- NULL
+        }
+        
+        ## create a method for getting the matrix
+        get <- function() {
+                ## Returning matrix
+                matx
+        }
+        
+        ## create a method to generate the inverse of matrix
+        setInv <- function(inverse) {
+                inv <<- inverse
+        }
+        
+        ## create a method to get the inverse of matrix
+        getInv <- function() {
+                ## Return the inverse property
+                inv
+        }
+        
+        ## list of the methods
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
+## generate inverse of the matrix given by "makeCacheMatrix"
+## If the inverse already exists then the "cachesolve" should load the 
+## inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## Return inverse of 'x'
+        matx <- x$getInv()
+        
+        ## return the inverse if already exists
+        if( !is.null(matx) ) {
+                print("use stored matrix")
+                return(matx)
+        }
+        
+        ## Get the matrix 
+        newmatx <- x$get()
+        
+        ## Calculate inverse 
+        matx <- solve(newmatx) %*% newmatx
+        
+        ## Set inverse 
+        x$setInv(matx)
+        
+        ## Return  matrix
+        matx
 }
